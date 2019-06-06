a4bdacaaaced3a842e5719f6d2a07c022cff308c
kalaikannan26s <51432038+kalaikannan26s@users.noreply.github.com>
2019-06-06 16:07:29 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+a1=input()
+if((a1=='e')or(a1=='a1')or(a1=='i')or(a1=='o')or(a1=='u')):
+    print("a1lpha1bet")
+elif((a1=='b')or(a1=='c')or(a1=='d')or(a1=='f')or(a1=='g')or(a1=='h')or(a1=='j')or(a1=='k')or(a1=='l')or(a1=='m')or(a1=='n')or(a1=='p')or(a1=='q')or(a1=='r')or(a1=='s')or(a1=='t')or(a1=='v')or(a1=='w')or(a1=='x')or(a1=='y')or(a1=='z')):
+    print("a1lpha1bet")
+else:
+    print("No")
