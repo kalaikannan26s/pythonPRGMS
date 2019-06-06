c9a37cf8b80205558b1e73f1fbb376d2aefbc910
30jawahar <42028068+30jawahar@users.noreply.github.com>
2019-06-06 12:33:10 +0530

    Update la.py

--- la.py
+++ la.py
@@ -1,10 +1,3 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if((a>b)and(a>c)):
-    print("a")
-elif((b>a)and(b>c)):
-    print("b")
-else:
-    print("c")
-    
+a,b,c=input().split()
+print(max(a,b,c,key=int))
+
