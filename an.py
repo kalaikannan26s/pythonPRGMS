878fe877dde9dde853720cd58658e9c92aebbca3
30jawahar <42028068+30jawahar@users.noreply.github.com>
2019-06-06 12:25:33 +0530

    Update an.py

--- an.py
+++ an.py
@@ -1,8 +1,8 @@
 a=input()
 if((a=='e')or(a=='a')or(a=='i')or(a=='o')or(a=='u')):
-    print("alphabet")
+    print("Alphabet")
 elif((a=='b')or(a=='c')or(a=='d')or(a=='f')or(a=='g')or(a=='h')or(a=='j')or(a=='k')or(a=='l')or(a=='m')or(a=='n')or(a=='p')or(a=='q')or(a=='r')or(a=='s')or(a=='t')or(a=='v')or(a=='w')or(a=='x')or(a=='y')or(a=='z')):
-    print("alphabet")
+    print("Alphabet")
 else:
-    print("not alphabet")
+    print("No")
 
